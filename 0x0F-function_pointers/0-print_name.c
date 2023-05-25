c03818260a929b4004843108b6274c70cd2ff1fb
MaryomaElsayed <maryamelsayd.1@gmail.com>
2023-05-24 17:50:12 -0700

    jsjc

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,10 +6,10 @@
  * @f: pointer to function
  * Return: nothing
  **/
-void print_name(char *roma, void (*f)(char *))
+void print_name(char *name, void (*f)(char *))
 {
-	if (roma == NULL || f == NULL)
+	if (name == NULL || f == NULL)
 		return;
 
-	f(roma);
+	f(name);
 }
