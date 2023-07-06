ab850a1e3edc9aa3f3691993bc9620ec562e273e
root <maryamelsayd.1@gmail.com>
2023-07-06 15:27:11 -0700

    jide

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,17 +2,19 @@
 
 /**
  * get_bit - returns the value of a bit at an index in a decimal number
- * @n: number to search search ohhhhhh
- * @index: index of the bit nisn
+ * @n: number to search
+ * @index: index of the bit
  *
  * Return: value of the bit
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
 	int bit_val;
-	if(index > 63)
+
+	if (index > 63)
 		return (-1);
-	bit_val=(n >> index) & 1;
+
+	bit_val = (n >> index) & 1;
+
 	return (bit_val);
 }
-
