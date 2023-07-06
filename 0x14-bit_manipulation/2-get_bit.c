2b1a1ceeadee03f05fa4b2e571871045af0b863f
root <maryamelsayd.1@gmail.com>
2023-07-06 15:52:02 -0700

    nijs

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,19 +2,19 @@
 
 /**
  * get_bit - returns the value of a bit at an index in a decimal number
- * @n: number to search
+ * @v: number to search
  * @index: index of the bit
  *
  * Return: value of the bit
  */
-int get_bit(unsigned long int n, unsigned int index)
+int get_bit(unsigned long int v, unsigned int index)
 {
 	int bit_val;
 
 	if (index > 63)
 		return (-1);
 
-	bit_val = (n >> index) & 1;
+	bit_val = (v >> index) & 1;
 
 	return (bit_val);
 }
