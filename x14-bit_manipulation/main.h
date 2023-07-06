f325dd373daf30c1d063a4bed2a236d50582f66f
root <maryamelsayd.1@gmail.com>
2023-07-06 14:51:10 -0700

    i9f

--- x14-bit_manipulation/main.h
+++ x14-bit_manipulation/main.h
@@ -1,9 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-//niisinisonosino iojmswdiwnep//
-//jijdn9osn9dnosn fewf//
-
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
@@ -15,4 +12,3 @@ int _putchar(char c);
 int get_endianness(void);
 
 #endif
-
